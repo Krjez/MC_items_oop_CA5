a99ec29847bfd8487d165573e3e3019dda06990d
Jakub Polacek <Kuba.krtek@email.cz>
2024-04-21 20:58:21 +0200

    Added Ruby's feature 11&13

--- src/main/java/ClientServer/Client.java
+++ src/main/java/ClientServer/Client.java
@@ -14,6 +14,8 @@ import DTOs.Block;
 
 public class Client
 {
+    private  DataOutputStream dataOutputStream = null;
+    private  DataInputStream dataInputStream = null;
     public static void main(String[] args)
     {
         Client client = new Client();
@@ -28,7 +30,7 @@ public class Client
             System.out.println("Client message: The Client is running and has connected to the server");
             Scanner keyboardInput = new Scanner(System.in);
             System.out.println("Please enter a command: ");
-            System.out.println("Available commands: F9, quit");
+            System.out.println("Available commands: F9, F11, F13 quit");
             String clientCommand = keyboardInput.nextLine();
 
             while(true)
@@ -43,7 +45,54 @@ public class Client
                     Gson gsonParser = new Gson();
                     Block block = gsonParser.fromJson(response,Block.class);
                     //TODO make output nicer
-                    System.out.println(block.toString());
+                    if(block != null)
+                    {
+                        System.out.println(block.toString());
+                    }
+                    else
+                    {
+                        System.out.println("Invalid ID.");
+                    }
+                }
+                else if (clientCommand.equals("F11"))
+                {
+                    System.out.println("Function 11 selected - Insert Block");
+                    Block blockToAdd = createBlock();
+
+                    Gson gsonParser = new Gson();
+                    String blockJson = gsonParser.toJson(blockToAdd);
+
+                    out.println("F11"+blockJson);
+                    String response = in.readLine();
+                    System.out.println(response);
+
+                    //by Ruby 20/4/2024
+                }
+                else if (clientCommand.equals("F13")) {
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    dataOutputStream = new DataOutputStream( socket.getOutputStream());
+                    // Here we call receiveFile define new for that file
+
+                    out.println("F13");
+                    System.out.println("All Images:");
+
+                    String response = in.readLine();
+
+                    System.out.println(response);
+
+                    Scanner key = new Scanner(System.in); //making a new scanner since using the same one with next line can be funky
+
+                    String imageName = key.nextLine() + ".png";
+
+                    out.println("img" + imageName);
+                    try {
+                        receiveFile("clientImages/" + imageName);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    dataInputStream.close();
+                    dataOutputStream.close();
                 }
                 else if (clientCommand.startsWith("quit"))
                 {
@@ -67,5 +116,72 @@ public class Client
             System.out.println("Client message: IOException: " + e);
         }
         System.out.println("Exiting client, server may still be running.");
+    }
+
+    //Made by Ruby
+    public static Block createBlock(){
+
+        Scanner key = new Scanner(System.in);
+
+        System.out.print("Enter the name (or enter \"Close\" to exit): ");
+        String name = key.nextLine();
+
+        Block newBlock = null;
+
+        if(!name.equalsIgnoreCase("close")) {
+
+            System.out.print("Enter the hardness: ");
+            double hardness = key.nextDouble();
+
+            System.out.print("Enter the blast resistance: ");
+            double blastResistance = key.nextDouble();
+
+            System.out.print("Is gravity affected? (0 = False, 1 = True): ");
+            boolean gravityAffected = (key.nextInt() == 0) ? false : true;
+
+            newBlock = new Block(name, hardness, blastResistance, gravityAffected);
+        }
+        return newBlock;
+    }
+    private void receiveFile(String fileName)
+            throws Exception
+    {
+        int bytes = 0;
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+
+
+        // DataInputStream allows us to read Java primitive types from stream e.g. readLong()
+        // read the size of the file in bytes (the file length)
+        long size = dataInputStream.readLong();
+        System.out.println("Server: file size in bytes = " + size);
+
+
+        // create a buffer to receive the incoming bytes from the socket
+        byte[] buffer = new byte[4 * 1024];         // 4 kilobyte buffer
+
+        System.out.println("Server:  Bytes remaining to be read from socket: ");
+
+        // next, read the raw bytes in chunks (buffer size) that make up the image file
+        while (size > 0 &&
+                (bytes = dataInputStream.read(buffer, 0,(int)Math.min(buffer.length, size))) != -1) {
+
+            // above, we read a number of bytes from stream to fill the buffer (if there are enough remaining)
+            // - the number of bytes we must read is the smallest (min) of: the buffer length and the remaining size of the file
+            //- (remember that the last chunk of data read will usually not fill the buffer)
+
+            // Here we write the buffer data into the local file
+            fileOutputStream.write(buffer, 0, bytes);
+
+            // reduce the 'size' by the number of bytes read in.
+            // 'size' represents the number of bytes remaining to be read from the socket stream.
+            // We repeat this until all the bytes are dealt with and the size is reduced to zero
+            size = size - bytes;
+            System.out.print(size + ", ");
+        }
+
+        System.out.println("File is Received");
+
+        System.out.println("Look in the \"clientImages\" folder to see the transferred image.");
+        fileOutputStream.close();
     }
 }
--- src/main/java/ClientServer/ClientHandler.java
+++ src/main/java/ClientServer/ClientHandler.java
@@ -1,9 +1,6 @@
 package ClientServer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import DAOs.BlockDaoInterface;
 import DAOs.MySqlBlockDao;
@@ -16,6 +13,8 @@ import DAOs.MySqlBlockDao;
 
 public class ClientHandler implements Runnable
 {
+    private  DataOutputStream dataOutputStream = null;
+    private  DataInputStream dataInputStream = null;
     BufferedReader clientReader;
     PrintWriter clientWriter;
     Socket clientSocket;
@@ -54,6 +53,51 @@ public class ClientHandler implements Runnable
                     clientWriter.println(blockAsJson);
                     System.out.println("Server message: JSON string of Block by id " + message + " sent to client.");
                 }
+                //by Ruby 20/4/2024
+                else if(request.substring(0,3).equals("F13")){
+                    // https://stackoverflow.com/a/5694398 referenced on 20/04/2024
+                    File folder = new File("serverImages");
+                    File[] listOfFiles = folder.listFiles();
+
+                    String allImages = "";
+
+
+                    if(listOfFiles != null) {
+                        for (int i = 0; i < listOfFiles.length; i++) {
+                            System.out.println(listOfFiles[i].getName());
+                            if (listOfFiles[i].isFile()) {
+                                allImages += listOfFiles[i].getName().substring(0, listOfFiles[i].getName().length()-4) + ((i != listOfFiles.length-1) ? " - " : "");
+                            }
+                        }
+                    }
+
+                    clientWriter.println(allImages);
+                }
+                //by Ruby 20/4/2024
+                else if(request.substring(0,3).equals("img")){
+
+                    dataInputStream = new DataInputStream(clientSocket.getInputStream());
+                    dataOutputStream = new DataOutputStream( clientSocket.getOutputStream());
+                    // Call SendFile Method
+                    String imgPath = "serverImages/" + request.substring(3);
+                    File f = new File(imgPath);
+                    if(f.exists() && !f.isDirectory()) { //https://stackoverflow.com/a/1816676 on the 20/4/2024
+                        try {
+                            System.out.println("Successfully sent image!");
+                            sendFile(imgPath);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    else{
+                        try {
+                            System.out.println("Sent a default image since the file \"" + imgPath + "\" does not exist.");
+                            sendFile("serverImages/noImage.png"); //sends default image if one does not exist
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
                 else if (request.startsWith("quit"))
                 {
                     clientWriter.println("Sorry to see you leaving. Goodbye.");
@@ -84,5 +128,32 @@ public class ClientHandler implements Runnable
             }
         }
         System.out.println("Server: (ClientHandler): Handler for Client " + clientNumber + " is terminating .....");
+    }
+
+    /**
+     * Made by Ruby
+     */
+    private void sendFile(String path)
+            throws Exception
+    {
+        int bytes = 0;
+        // Open the File at the specified location (path)
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // send the length (in bytes) of the file to the server
+        dataOutputStream.writeLong(file.length());
+
+        // Here we break file into chunks
+        byte[] buffer = new byte[4 * 1024]; // 4 kilobyte buffer
+
+        // read bytes from file into the buffer until buffer is full or we reached end of file
+        while ((bytes = fileInputStream.read(buffer))!= -1) {
+            // Send the buffer contents to Server Socket, along with the count of the number of bytes
+            dataOutputStream.write(buffer, 0, bytes);
+            dataOutputStream.flush();   // force the data into the stream
+        }
+        // close the file
+        fileInputStream.close();
     }
 }
